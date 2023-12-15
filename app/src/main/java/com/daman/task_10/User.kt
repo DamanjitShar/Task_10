4b4254d93895f374e19d4e28861e09420086aa71
Damanjit <damanjit1993@outlook.com>
2023-12-14 20:59:24 -0700

    Task 10

---
+++ app/src/main/java/com/daman/task_10/User.kt
@@ -0,0 +1,7 @@
+package com.daman.task_10
+
+data class User(
+    var username: String,
+    var email: String,
+    var phoneNumber: String
+)
---
+++ app/src/main/java/com/daman/task_10/UserAdapter.kt
@@ -0,0 +1,55 @@
+package com.daman.task_10
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapter(
+    private val userList: MutableList<User>,
+    private val onItemClickListener: (User) -> Unit
+) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = userList[position]
+        holder.bind(user)
+        holder.itemView.setOnClickListener { onItemClickListener(user) }
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    fun addUser(user: User) {
+        userList.add(user)
+        notifyDataSetChanged()
+    }
+
+    fun updateUser(user: User, position: Int) {
+        userList[position] = user
+        notifyDataSetChanged()
+    }
+
+    fun deleteUser(position: Int) {
+        userList.removeAt(position)
+        notifyDataSetChanged()
+    }
+
+    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val usernameTextView: TextView = itemView.findViewById(R.id.usernameTextView)
+        private val emailTextView: TextView = itemView.findViewById(R.id.emailTextView)
+        private val phoneNumberTextView: TextView = itemView.findViewById(R.id.phoneNumberTextView)
+
+        fun bind(user: User) {
+            usernameTextView.text = user.username
+            emailTextView.text = user.email
+            phoneNumberTextView.text = user.phoneNumber
+        }
+    }
+}
